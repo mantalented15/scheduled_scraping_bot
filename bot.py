41aa7af82fdfd0fd3a013f41c470ee5522985282
unknown <mantalented15@gmail.com>
2022-11-10 01:24:00 +0800

    logging in with user and passwd succeed

--- bot.py
+++ bot.py
@@ -3,18 +3,30 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 import time
-import requests
+# import requests
 
-driver = webdriver.Chrome('chromedriver')
-driver.get('https://partner.outsourceaccelerator.com/login')
+# chrome_options= Options()
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("start-maximized")
+# chrome_options.add_argument("--disable-blink-features")
+# chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 
-try:
-    email=WebDriverWait(driver,60).until(EC.presence_of_element_located(By.ID, "email"))
-except:
-    print("Timed out waiting for page to load")
+LOGIN_PAGE="https://partner.outsourceaccelerator.com/login"
+ACCOUNT="partner.wingassistant@outsourceaccelerator.com"
+PASSWORD="Os@!!hgrj3138"
 
-# time.sleep(80)
+# driver=webdriver.Chrome('chromedriver', chrome_options=chrome_options)
+driver=webdriver.Chrome('chromedriver')
+
+wait=WebDriverWait(driver, 30)
+driver.get(LOGIN_PAGE)
+wait.until(EC.element_to_be_clickable((By.ID, "email"))).send_keys(ACCOUNT)
+wait.until(EC.element_to_be_clickable((By.ID, "password"))).send_keys(PASSWORD)
+wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Log in']"))).click()
+
+time.sleep(30)
 # a = driver.find_elements_by_xpath('//li[@data-testid="list-item-8"]//div[@class="ant-space-item"]')
 # a = driver.find_elements_by_xpath('//li[@data-testid="list-item-8"]')
 # a = driver.find_element(By.XPATH, '//div[@class="ant-space-item"]/a')
