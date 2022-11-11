cd4d539779330d5dea407eaa9e837552d0a46457
unknown <mantalented15@gmail.com>
2022-11-11 21:17:36 +0800

    passing the test of scrolling the leads

--- bot.py
+++ bot.py
@@ -3,97 +3,141 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 import time
-
-options= Options()
-# options.add_argument("--headless")
-options.add_argument("start-maximized")
-# options.add_argument("--disable-blink-features")
-# options.add_argument("--disable-blink-features=AutomationControlled")
-
-LOGIN_PAGE="https://partner.outsourceaccelerator.com/login"
-ACCOUNT="partner.wingassistant@outsourceaccelerator.com"
-PASSWORD="Os@!!hgrj3138"
-LEAD_PAGE="https://partner.outsourceaccelerator.com/partner/leads"
-
-driver=webdriver.Chrome('chromedriver', options=options)
-# driver=webdriver.Chrome('chromedriver')
+import pandas as pd
 
 
-wait=WebDriverWait(driver, 30)
+def get_leads_data():
+    options= Options()
+    # options.add_argument("--headless")
+    # options.add_argument("start-maximized")
+    # options.add_argument("--disable-blink-features")
+    # options.add_argument("--disable-blink-features=AutomationControlled")
 
-#Login with account and password
-driver.get(LOGIN_PAGE)
+    LOGIN_PAGE="https://partner.outsourceaccelerator.com/login"
+    ACCOUNT="partner.wingassistant@outsourceaccelerator.com"
+    PASSWORD="Os@!!hgrj3138"
 
-wait.until(EC.element_to_be_clickable((By.ID, "email"))).send_keys(ACCOUNT)
-wait.until(EC.element_to_be_clickable((By.ID, "password"))).send_keys(PASSWORD)
-wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Log in"]'))).click()
+    driver=webdriver.Chrome('chromedriver', options=options)
+    # driver=webdriver.Chrome('chromedriver')
 
-#Load the leader page
-wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Inbound inquiries"]'))).click()
+    # options.binary_location=r'C:\Program Files (x86)\Mozilla Firefox\firefox.exe'
+    # driver=webdriver.Firefox(executable_path="geckodriver", options=options)
 
-wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@href="/partner/leads"]'))).click()
+    driver.maximize_window()
 
-    #loop for each lead and view
+    wait=WebDriverWait(driver, 30)
 
-    #Click view button for each lead
-    # wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@id="row-0"]//button[text()="View"]'))).click()
+    #Login with account and password
+    driver.get(LOGIN_PAGE)
+    wait.until(EC.element_to_be_clickable((By.ID, "email"))).send_keys(ACCOUNT)
+    wait.until(EC.element_to_be_clickable((By.ID, "password"))).send_keys(PASSWORD)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Log in"]'))).click()
 
-    # view_buttons = wait.until(EC.visibility_of_all_elements_located((By.XPATH, '//div[@role="row"]//button[text()="View"]')))
+    #Load the leader page
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Inbound inquiries"]'))).click()
 
-    # for index in range(len(view_buttons)):
-    #     # print(index)
-    #     wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="row-%d"]//button[text()="View"]'%index))).click()
-    #     # wait.until(EC.element_located_to_be_selected((By.XPATH, '//div[@id="row-%d"]//button[text()="View"]'%index))).click()
-
-    #     # view.click()
-    #     wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Close panel"]/ancestor::button'))).click()
-
-    #javascript execution for zoom in to fix unexpected errors
-    # script= "document.body.style.zoom='30%'"
-    # driver.execute_script(script)
-    # time.sleep(3)
-    # time.sleep(8)
-    # element=driver.find_element(By.XPATH, "//div[@id='row-4' and @role='row']//button")
-    # driver.execute_script("arguments[0].scrollIntoView();", element)
-    # element.location_once_scrolled_into_view
-    # element.click()
-
-    # element=wait.until(EC.presence_of_element_located((By.XPATH, '//div[@id="row-4" and @role="row"]//button')))
-    # print(element)
-    # action=ActionChains(driver)
-    # action.move_to_element(element).click().perform()
-
-    # wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@id="row-0" and @role="row"]//button'))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@href="/partner/leads"]'))).click()
 
 
+    #click the view0
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@id="row-0" and @role="row"]//button'))).click()
 
-    # count=0
-    # for view in view_buttons:
-    #     count=count+1
-    #     print(count)
-    #     print(view)
-    #     view.click()
-    #     wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Close panel"]/ancestor::button'))).click()
-    #     time.sleep(3)
-time.sleep(20)
+    #get the data under lead
+    divs = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'dl > div')))
 
-#view the lead
-wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@id="row-0" and @role="row"]//button'))).click()
+    columns=['Lead score', 'Email address', 'Phone number', 'Timezone', 'Country', 'Device', 'Sector', 'Company size', 'Role to outsource', 'Number of staff to oursource', 'Key question', 'Comment', 'Data added']
+    dict_lead_data={}
+    leads_data=[]
+    for index in range(len(divs)):
+        dict_lead_data[columns[index]]=divs[index].find_element(By.TAG_NAME, 'dd').text
+    leads_data.append(dict_lead_data)
 
-#get the data under lead
-divs = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'dl > div')))
+    #convert leads_data into panda dataframe
+    df_lead_data=pd.DataFrame(leads_data)
 
-for div in divs:
-    lead_score=div.find_element(By.TAG_NAME, 'dd').text
-    print(lead_score)
-# lead_score
+    # print(df_lead_data)
 
-time.sleep(20)
-#Cancel the view
-wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Close panel"]/ancestor::button'))).click()
+    time.sleep(3)
+    #Cancel the view
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Close panel"]/ancestor::button'))).click()
 
 
-time.sleep(30)
+    #view1
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//div[@id="row-1" and @role="row"]//button'))).click()
+    time.sleep(3)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Close panel"]/ancestor::button'))).click()
 
+    #test lead
+            # last_height = driver.execute_script("return document.body.scrollHeight")
+    driver.execute_script("window.scrollTo(0, 150);")
+    time.sleep(5)
+    # element = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="row-2" and @role="row"]//button')))
+    element=driver.find_element(By.XPATH, "//div[@id='row-2' and @role='row']//button")
+    print(element)
+    action=ActionChains(driver)
+    action.move_to_element(element).perform()
+    time.sleep(1)
+    element.click()
+ 
+    time.sleep(3)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Close panel"]/ancestor::button'))).click()
+
+
+    #test lead
+            # last_height = driver.execute_script("return document.body.scrollHeight")
+    driver.execute_script("window.scrollTo(0, 150);")
+    time.sleep(5)
+    # element = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="row-2" and @role="row"]//button')))
+    element=driver.find_element(By.XPATH, "//div[@id='row-2' and @role='row']//button")
+    print(element)
+    action=ActionChains(driver)
+    action.move_to_element(element).perform()
+    time.sleep(1)
+    element.click()
+ 
+    time.sleep(3)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Close panel"]/ancestor::button'))).click()
+
+    #test lead
+            # last_height = driver.execute_script("return document.body.scrollHeight")
+    driver.execute_script("window.scrollTo(0, 200);")
+    time.sleep(5)
+    # element = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="row-2" and @role="row"]//button')))
+    element=driver.find_element(By.XPATH, "//div[@id='row-3' and @role='row']//button")
+    print(element)
+    action=ActionChains(driver)
+    action.move_to_element(element).perform()
+    time.sleep(1)
+    element.click()
+ 
+    time.sleep(3)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Close panel"]/ancestor::button'))).click()
+
+    #test lead
+            # last_height = driver.execute_script("return document.body.scrollHeight")
+    driver.execute_script("window.scrollTo(0, 400);")
+    time.sleep(5)
+    # element = wait.until(EC.visibility_of_element_located((By.XPATH, '//div[@id="row-2" and @role="row"]//button')))
+    element=driver.find_element(By.XPATH, "//div[@id='row-4' and @role='row']//button")
+    print(element)
+    action=ActionChains(driver)
+    action.move_to_element(element).perform()
+    time.sleep(1)
+    element.click()
+ 
+    time.sleep(3)
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Close panel"]/ancestor::button'))).click()
+
+
+
+    time.sleep(30)
+
+
+def main():
+    get_leads_data()
+
+if __name__ == "__main__":
+    main()
