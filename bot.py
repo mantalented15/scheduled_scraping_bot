51bed49de50b764cc9d51a139a5543686d0b3cbc
unknown <mantalented15@gmail.com>
2022-11-15 09:36:56 +0800

    handling gitignore files

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 # from selenium.webdriver.common.action_chains import ActionChains
 import time
 import pandas as pd
-
+import config
 
 def get_leads_data():
     options= Options()
@@ -15,10 +15,6 @@ def get_leads_data():
     # options.add_argument("start-maximized")
     # options.add_argument("--disable-blink-features")
     # options.add_argument("--disable-blink-features=AutomationControlled")
-
-    LOGIN_PAGE="https://partner.outsourceaccelerator.com/login"
-    ACCOUNT="partner.wingassistant@outsourceaccelerator.com"
-    PASSWORD="Os@!!hgrj3138"
 
     driver=webdriver.Chrome('chromedriver', options=options)
     # driver=webdriver.Chrome('chromedriver')
@@ -31,9 +27,9 @@ def get_leads_data():
     wait=WebDriverWait(driver, 30)
 
     #Login with account and password
-    driver.get(LOGIN_PAGE)
-    wait.until(EC.element_to_be_clickable((By.ID, "email"))).send_keys(ACCOUNT)
-    wait.until(EC.element_to_be_clickable((By.ID, "password"))).send_keys(PASSWORD)
+    driver.get(config.login_page)
+    wait.until(EC.element_to_be_clickable((By.ID, "email"))).send_keys(config.account)
+    wait.until(EC.element_to_be_clickable((By.ID, "password"))).send_keys(config.password)
     wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Log in"]'))).click()
 
     #Load the leader page
@@ -43,27 +39,36 @@ def get_leads_data():
     #get the number of leads per page
     number_per_page = int(wait.until(EC.visibility_of_element_located((By.XPATH, '//select//option[@selected]'))).get_attribute("value"))
 
-    # get the number of total leads
+    # get the number of total leads and the number of leads of last page
     caption_leads_number = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[contains(text(), 'of')]"))).text
     leads_number = int(caption_leads_number[caption_leads_number.find('of')+2:])
+    leads_number_last_page = leads_number % number_per_page
 
     # calcuate the number of pages
     pages_number = int(leads_number/number_per_page) + 1
 
     #calculate the scroll step
     scroll_height = driver.execute_script("return document.body.scrollHeight")
-    scroll_step = scroll_height/number_per_page
+    # scroll_step = int(scroll_height/(number_per_page))
+    scroll_step = 100
 
     #click the view buttons of leads and get the data into dataframe
     df_lead_data=pd.DataFrame()
 
     for page_index in range(pages_number):
         driver.execute_script("window.scrollTo(0, 0);")
-        time.sleep(2)
-        for lead_index in range(number_per_page):
+        time.sleep(3)
+
+        if page_index == pages_number-1:
+            number_in_page = leads_number_last_page
+        else:
+            number_in_page = number_per_page
+
+        for lead_index in range(number_in_page):
             print(lead_index)
-            if lead_index:
+            if lead_index > 0:
                 driver.execute_script("window.scrollTo(0, arguments[0]);", scroll_step*lead_index)
+                time.sleep(0.5)
             wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@id='row-{}' and @role='row']//button".format(lead_index)))).click()
 
             #get the data under lead
@@ -81,11 +86,12 @@ def get_leads_data():
             wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Close panel"]/ancestor::button'))).click()
 
         #go over the next page
-        if page_index!=pages_number-1:  # do not go over next page in case of last page
+        if page_index!=pages_number-1:  # go over next page if not last page
             wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Next Page']"))).click()
+            time.sleep(3)
 
     print(df_lead_data)
-
+    df_lead_data.to_csv('leads_data.csv', index=False)
 
 def main():
     get_leads_data()
